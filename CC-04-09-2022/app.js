1d6d4ce03259abbf3eb3c13eab6ad9a354909278
Devin Murphy <devinmurphy-webdev@gmail.com>
2022-04-12 18:45:31 -0400

    Coding challenge and solution 2022-04-09

--- CC-04-09-2022/app.js
+++ CC-04-09-2022/app.js
@@ -1,16 +1,3 @@
-// Convert number to reversed array of digits
-// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
-
-// Example:
-// 348597 => [7,9,5,8,4,3]
-// 0 => [0]
-
-//P: Are you ever given an empty array or undefined? What then? Are you always give digits? 
-//R: Whatever number was given it is now split up into an array and the order of the numbers is reversed. 
-//E: if we were given 9238=> [8,3,2,9]
-//                    330=> [0,3,3]
-//                    undefined => not a number 
-//P: make a funciton that takes in a number 
 function digitize(num){
     //make number a string and split it 
     //reverse array
@@ -20,3 +7,4 @@ function digitize(num){
 
 digitize(9238) //should return [8,3,2,9]
 console.log(digitize(330),[0,3,3])
+// can't do it this way. Have to use map. woof
