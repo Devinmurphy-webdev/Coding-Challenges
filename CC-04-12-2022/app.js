2843713cc97de47316e7b202c020dc6e6d6cf7b4
Devin Murphy <devinmurphy.webdev@gmail.com>
2022-04-12 19:16:44 -0400

    coding challenge and solution for 04-12-2022

---
+++ CC-04-12-2022/app.js
@@ -0,0 +1,7 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function evenOdd(number){
+    return number % 2 ? "Odd" : "Even"
+}
+
+evenOdd(2)
+evenOdd(11)
