7d6e994e5bda5baee845fa171e2ba824d871074f
Devin Murphy <devinmurphy.webdev@gmail.com>
2022-04-22 09:05:58 -0400

    Coding challenge and solution for 04-22-2022

---
+++ CC-04-22-2022/app.js
@@ -0,0 +1,21 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+//Parameters: 
+//Return: 
+//Example:
+//Pseudo: 
+function opposite(number) {
+    let oppNum = 0 - number
+    return oppNum;
+}
+
+opposite(1)
+opposite(14)
+opposite(-34)
